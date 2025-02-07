f978a1a72001cc6d5181b5a5e428cacd6b3a7b38
Sadok <kouassisadok9@gmail.com>
2025-02-07 13:44:08 +0100

    bb

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import About from "./components/About";
-import Book from "./components/book";
+import Book from "./components/Book";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
